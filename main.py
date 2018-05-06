90db382df25c47ab679a9774fcf184f47eb5b5dd
Alireza <daban.am56@gmail.com>
2018-05-06 18:04:31 +0430

    resource_manager completed

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from src import resource_manager
 
 resource_manager.load_resources(
     professor_skills="resources/Prof_Skill.xlsx",
-    classes="resources/Amoozesh.xlsx"
+    classes="resources/Amoozesh.xlsx",
+    subjects="resources/subjects.xlsx",
+    free_times="resources/Proffosor_FreeTime.xlsx"
 )
 
 
--- src/resource_manager.py
+++ src/resource_manager.py
@@ -4,11 +4,15 @@ from functools import reduce
 professor_skills_map = None
 lessons_list = None
 classes_list = None
+subject_list = None
+free_times_map = None
 
 
-def load_resources(professor_skills, classes):
+def load_resources(professor_skills, classes, subjects, free_times):
     load_professor_skills(professor_skills_file_name=professor_skills)
     load_classes(classes_file_name=classes)
+    load_subjects(subject_file_name=subjects)
+    load_free_times(free_times_file_name=free_times)
 
 
 def load_professor_skills(professor_skills_file_name):
@@ -35,11 +39,15 @@ def load_professor_skills(professor_skills_file_name):
         max_col=prof_skills_sheet.max_column
     )
 
-    lessons_list = []
-
-    for item in lessons:
-        for lesson in item:
-            lessons_list.append(lesson.value)
+    lessons_list = list(
+        map(
+            lambda x: x.value,
+            reduce(
+                lambda x, y: x.append(y),
+                [row for row in lessons]
+            )
+        )
+    )
 
 
 def get_professor_skills(professor):
@@ -79,3 +87,71 @@ def get_classes():
     global classes_list
 
     return classes_list
+
+
+def load_subjects(subject_file_name):
+    global subject_list
+
+    subjects_work_book = load_workbook(subject_file_name)
+    subject_sheet = subjects_work_book.active
+
+    subjects = subject_sheet.iter_rows(
+        min_row=1,
+        min_col=1,
+        max_row=subject_sheet.max_row,
+        max_col=subject_sheet.max_column
+    )
+
+    subject_list = list(
+        map(
+            lambda x: x.value,
+            reduce(
+                lambda x, y: x.append(y),
+                [row for row in subjects]
+            )
+        )
+    )
+
+
+def get_subjects():
+    global subject_list
+
+    return subject_list
+
+
+def load_free_times(free_times_file_name):
+    global free_times_map
+
+    free_times_work_book = load_workbook(free_times_file_name)
+
+    free_times_map = {}
+
+    for professor_free_time_sheet in free_times_work_book:
+        sheet_rows = professor_free_time_sheet.iter_rows(
+            min_row=2,
+            min_col=2,
+            max_row=professor_free_time_sheet.max_row,
+            max_col=professor_free_time_sheet.max_column
+        )
+
+        professor_week_free_time = [
+            tuple(
+                [time_slot.value for time_slot in row]
+            ) for row in sheet_rows
+        ]
+
+        free_times_map.update(
+            {
+                professor_free_time_sheet.title:
+                    tuple(professor_week_free_time)
+            }
+        )
+
+
+def get_professor_free_time(professor):
+    global free_times_map
+
+    if free_times_map is not None:
+        return free_times_map.get(professor, None)
+    else:
+        return None
